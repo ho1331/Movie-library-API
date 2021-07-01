0cf07e659703caa90aa2f3ef7fbef4a276c820d0
Pavel Braha <strangelancer@gmail.com>
2021-07-02 00:13:42 +0300

    create tables

--- proj/src/main.py
+++ proj/src/main.py
@@ -1,6 +1,9 @@
 from flask_restful import Api
 
 from src.app import create_app
+from src.views.directors_views import DirectorsList
+from src.views.films_views import FilmsList
+from src.views.genres_views import GenresList
 from src.views.users_views import Users, UsersList
 
 app = create_app()
@@ -9,3 +12,6 @@ app = create_app()
 api = Api(app)
 api.add_resource(UsersList, "/api/users/")
 api.add_resource(Users, "/api/users/<int:id>/")
+api.add_resource(DirectorsList, "/api/directors/")
+api.add_resource(GenresList, "/api/genres/")
+api.add_resource(FilmsList, "/api/films/")
