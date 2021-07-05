d5c223b9c074e11dc6043cd02a9d95e20c28b7cf
Pavel Braha <strangelancer@gmail.com>
2021-07-06 00:22:40 +0300

    created autorizations

--- proj/src/views/login_views.py
+++ proj/src/views/login_views.py
@@ -1,13 +1,57 @@
-from flask import Blueprint, render_template, request
-from flask_login import current_user, login_user
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 from src.forms.loginform import LoginForm
+from src.models.users import User
 
-sign = Blueprint("sign", __name__)
+
+class LoginApi(Resource):
+    def post(self):
+        body = request.get_json()
+        user = User.query.filter_by(email=body.get("login")).first()
+        authorized = user.check_password(body.get("password"))
+        if not authorized:
+            return {"error": "Email or password invalid"}, 401
+
+        login_user(user, remember=True)
+        return redirect("/api/films/")
+
+    def get(self):
+        if current_user.is_authenticated:
+            return redirect("/api/films/")
+        # here will be sign(registration) form
+        form = {"login": "", "password": ""}
+        return form, 200
 
 
-@sign.route("/")
-def login():
-    form = LoginForm()
-    return render_template("login.html", title="Sign In", form=form)
+class LogoutApi(Resource):
+    @login_required
+    def get(self):
+        logout_user()
+        return redirect("/api/done/")
+
+
+# sign = Blueprint("sign", __name__)
+
+
+# @sign.route("/", methods=["GET", "POST"])
+# def login():
+#     form = LoginForm()
+#     if current_user.is_authenticated:
+#         return redirect("/api/films/")
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(email=form.login.data).first()
+#         if user is None or not user.check_password(form.password.data):
+#             flash("Invalid username or password")
+#             return redirect("/api/login/")
+#         login_user(user, remember=form.remember_me.data)
+#         return redirect("/api/films/")
+#     return render_template("login.html", title="Sign In", form=form)
+
+
+# @login_required
+# @sign.route("/logout")
+# def logout():
+#     logout_user()
+#     return redirect("/api/done/")
