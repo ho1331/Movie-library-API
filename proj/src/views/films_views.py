d2cf88c6cbfe5ed189d26592eef2908d03224eda
Pavel Braha <strangelancer@gmail.com>
2021-07-15 18:56:59 +0300

    add patch method in FilmsItem

--- proj/src/views/films_views.py
+++ proj/src/views/films_views.py
@@ -2,6 +2,8 @@ from flask import request
 from flask_login import login_required  # decorator для ограничения доступа к методу
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+from src.app import db
+from src.models.directors import Director
 
 ##
 from src.models.films import Film
@@ -180,6 +182,8 @@ class FilmsListViews(Resource):
                 description: user nick
 
         """
+        # partial match search
+        part = request.args.get("part")
         # filters_params
         rating = request.args.get("rating")
         director_id = request.args.get("director-id")
@@ -189,7 +193,9 @@ class FilmsListViews(Resource):
         sort_by_rating = request.args.get("sort-by-rating")
         sort_by_release = request.args.get("sort-by-release")
         # filters
-        if rating:
+        if part:
+            films = Film.query.filter(Film.title.like(f"%{part}%")).all()
+        elif rating:
             films = Film.query.filter_by(rating=rating)
         elif director_id:
             films = Film.query.filter_by(director_id=director_id)
@@ -216,7 +222,7 @@ class FilmsListViews(Resource):
                 "id": film.id,
                 "title": film.title,
                 "release": str(film.release),
-                "director": f"{film.directors.name} {film.directors.sername}",  # f"{film.directors.query.first().name} {film.directors.query.first().sername}",
+                "director": f"{film.directors.name} {film.directors.sername}",
                 "description": film.description,
                 "rating": film.rating,
                 "poster": film.poster,
@@ -228,7 +234,34 @@ class FilmsListViews(Resource):
         return serialized_data, 200
 
 
-class FilmsDel(Resource):
+class FilmsItem(Resource):
+    def patch(self, id):
+        request_json = request.get_json(silent=True)
+        director = request_json.get("director")
+        request_genre = request_json.get("genres")
+        Film.query.filter_by(id=id).update(
+            {
+                "title": request_json.get("title"),
+                "release": request_json.get(str("release")),
+                "director_id": Film.get_or_crete(
+                    Director, name=director[0], sername=director[1]
+                ).id,
+                "description": request_json.get("description"),
+                "rating": request_json.get("rating"),
+                "poster": request_json.get("poster"),
+            }
+        )
+        film = db.session.query(Film).get(id)
+        film.genres = []
+        for genre in request_genre:
+            # check if Genre is already exist
+            new_genre = Film.get_or_crete(Genre, genre=genre)
+            film.genres.append(new_genre)
+
+        db.session.commit()
+        return f"Success. Film with id={id} was updated", 200
+
+    @login_required
     def delete(self, id):
         """
         ---
