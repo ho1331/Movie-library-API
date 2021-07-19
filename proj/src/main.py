e60b3b277a6e45f9975ef6227a27e0595ec37ea5
Pavel Braha <strangelancer@gmail.com>
2021-07-19 16:23:00 +0300

    delete errors views

--- proj/src/main.py
+++ proj/src/main.py
@@ -4,7 +4,6 @@ from flask_restful import Api
 from src.app import app
 from src.tools.inserting import after_create
 from src.views.directors_views import DirectorsItem, DirectorsList
-from src.views.errors_views import Done, Errors
 from src.views.films_views import FilmsItem, FilmsList, FilmsListViews
 from src.views.genres_views import GenresList
 from src.views.login_views import LoginApi, LogoutApi
@@ -22,8 +21,6 @@ api.add_resource(GenresList, "/api/genres/")
 api.add_resource(FilmsList, "/api/films/")
 api.add_resource(FilmsListViews, "/api/films-views/")
 api.add_resource(FilmsItem, "/api/films-views/<int:id>")
-api.add_resource(Errors, "/api/error/")
-api.add_resource(Done, "/api/done/")
 api.add_resource(LoginApi, "/api/login/")
 api.add_resource(LogoutApi, "/api/logout/")
 api.add_resource(RefList, "/api/genre-film/")
